fc746082ed437e5029c82ac7478d728c7428f5ba
zprl <zprl.dev@gmail.com>
2024-04-11 21:32:17 +0900

    up

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,7 +1,6 @@
 // "Link":"Name"
 import { base } from "$app/paths";
 
-
 export const routeListToRoutes = (OBJ: object, BASE: string) => {
 	return Object.entries(OBJ)
 		.map((list) => [`${BASE}${list[0]}`, list[1]])
@@ -13,21 +12,20 @@ export const hiddenRouteList = {
 	"/hidden-tester": "TEST PAGE",
 	"/post": "Post",
 };
+export const hiddenRoutes = routeListToRoutes(hiddenRouteList, hiddenBase);
 
-const toolsBase = `${base}/tools`
+const toolsBase = `${base}/tools`;
 export const toolsRouteList = {
 	"/": "Mon tools",
 	"/color/watcher": "Color Watcher",
 	"/other/key-tester": "Key tester",
 };
-
-
-export const hiddenRoutes = Object.entries(hiddenRouteList)
-	.map((list) => [`${hiddenBase}${list[0]}`, list[1]])
-	.map(([link, title]) => ({ link, title }));
+export const toolsRoutes = routeListToRoutes(toolsRouteList, toolsBase);
 
 // export const routes = Object.entries(routeList)
 // 	.map((list) => [`${base}${list[0]}`, list[1]])
 // 	.map(([link, title]) => ({ link, title }));
 
-export const toolsRoutes = routeListToRoutes(toolsRouteList, toolsBase);
+// export const hiddenRoutes = Object.entries(hiddenRouteList)
+// 	.map((list) => [`${hiddenBase}${list[0]}`, list[1]])
+// 	.map(([link, title]) => ({ link, title }));

92da91305c627c464a6a930d4099305f655406f0
zprl <zprl.dev@gmail.com>
2024-05-05 19:01:56 +0900

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const BASE = import.meta.env.MODE === "dev" ? "" : process.env.BASE_PATH;
+const BASE = process.argv.includes("dev") ? "" : process.env.BASE_PATH;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {

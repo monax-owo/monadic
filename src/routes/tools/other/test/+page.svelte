b0fc92ce8f230533fd4f1ecb8627ddaa24f50aea
zprl <zprl.dev@gmail.com>
2024-05-11 20:57:01 +0900

    up

--- src/routes/tools/other/test/+page.svelte
+++ src/routes/tools/other/test/+page.svelte
@@ -3,4 +3,10 @@
 	import { allRoutes } from "$lib/route/routes";
 </script>
 
-<pre>{JSON.stringify(allRoutes, null, "\t")}</pre>
+<pre class="test">{JSON.stringify(allRoutes, null, "\t")}</pre>
+
+<style lang="postcss">
+	.test {
+		@apply bg-black;
+	}
+</style>

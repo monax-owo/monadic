88d474490c6bfb8d18b6b2c1ccdaffb2926d2e71
zprl <zprl.dev@gmail.com>
2024-05-17 19:57:49 +0900

    fix /style

---
+++ src/routes/style/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { base } from "$app/paths";
+	import { routeObjToRoutes } from "$lib/route/routes";
+
+	//
+	const allRoutes = routeObjToRoutes(
+		{
+			"/pixel": "Pixel Art CSS",
+		},
+		`${base}/style`,
+	);
+</script>
+
+<div class="flex flex-col">
+	{#each allRoutes as route}
+		<a href={route.link}>{route.title}</a>
+	{/each}
+</div>

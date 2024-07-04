a1585f681d82f5ecbaeba0fe35bd8330ab16924d
zprl <zprl.dev@gmail.com>
2024-07-04 20:20:17 +0900

    up

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,6 +6,8 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: "404.html",
+			edge: false,
+			split: false,
 		}),
 		alias: {
 			$routes: "src/routes",

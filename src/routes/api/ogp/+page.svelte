862541b7ebdcb164e95d9a4e7502d5ff8f99160b
zprl <zprl.dev@gmail.com>
2024-07-05 15:29:23 +0900

    up

--- src/routes/api/ogp/+page.svelte
+++ src/routes/api/ogp/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
   import { base } from "$app/paths";
-  import { page } from "$app/stores";
   import Template from "$lib/component/Template.svelte";
-  const title = "aaa";
+  const title = "test title";
 </script>
 
 <Template>
-  <!-- svelte-ignore a11y-missing-content -->
   <a href={`${base}/api/ogp/${encodeURIComponent(title)}.png`}>{title}</a>
   <!-- <img src={`/${base}/api/ogp/${encodeURIComponent(title)}.png`} alt={title} /> -->
 </Template>

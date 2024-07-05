cfaf6a78de1c1da1e3674a11678e9f41fcdfa5dd
zprl <zprl.dev@gmail.com>
2024-07-05 17:34:28 +0900

    up

--- svelte.config.js
+++ svelte.config.js
@@ -5,15 +5,11 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      fallback: "404.html",
       edge: false,
       split: false,
     }),
     alias: {
       $routes: "src/routes",
-    },
-    paths: {
-      base: process.argv.includes("dev") ? "/dev" : `/${process.env.BASE_PATH}`,
     },
   },
 };

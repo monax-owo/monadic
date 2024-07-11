48a2d0a2e8cd3145b5e2eb69b1f7e4da50e7946c
zprl <zprl.dev@gmail.com>
2024-07-11 09:59:58 +0900

    up

--- src/routes/tools/other/test-react/Page.tsx
+++ src/routes/tools/other/test-react/Page.tsx
@@ -1,7 +1,16 @@
+import { useState } from "preact/hooks";
+
 const Page = () => {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <button type="button">aaa</button>
+      <button
+        type="button"
+        onClick={() => {
+          setCount(count + 1);
+        }}>
+        aaa{count}
+      </button>
     </>
   );
 };

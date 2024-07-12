d3cf755f3e56db297df91fd4bf2c698af0ea37b6
zprl <zprl.dev@gmail.com>
2024-07-12 21:16:22 +0900

    up

--- src/lib/component/InputText.svelte
+++ src/lib/component/InputText.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
   import Button from "$lib/component/Button.svelte";
-  import { paste } from "$lib/util/clipboard";
   //
 </script>
 
 <div class="InputText">
   <input type="text" />
-  <Button on:click={(e) => paste()}>paste</Button>
+  <Button>paste</Button>
 </div>
 
 <style lang="scss">
-  .InputText {
-    //
-  }
+  // .InputText {
+  // }
 </style>
